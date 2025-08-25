a0bfd5a854f000d8da32822ef8391066f4ba120a
Matt Spurlin <mattsp1290@gmail.com>
2025-08-25 18:20:51 -0400

    sweetie16 palette with lipgloss colors

---
+++ pkg/color/color.go
@@ -0,0 +1,45 @@
+package color
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	BlackHexCode     = "#1a1c2c"
+	DarkGreyHexCode  = "#333c57"
+	GreyHexCode      = "#566c86"
+	LightGreyHexCode = "#94b0c2"
+	WhiteHexCode     = "#f4f4f4"
+
+	RedHexCode    = "#b13e53"
+	OrangeHexCode = "#ef7d57"
+	YellowHexCode = "#ffcd75"
+	GreenHexCode  = "#38b764"
+	BlueHexCode   = "#3b5dc9"
+	PurpleHexCode = "#5d275d"
+
+	LightGreenHexCode   = "#a7f070"
+	DarkGreenHexCode    = "#257179"
+	LightestBlueHexCode = "#73eff7"
+	LightBlueHexCode    = "#41a6f6"
+	DarkBlueHexCode     = "#29366f"
+
+	BlackColor     = lipgloss.Color(BlackHexCode)
+	DarkGreyColor  = lipgloss.Color(DarkGreyHexCode)
+	GreyColor      = lipgloss.Color(GreyHexCode)
+	LightGreyColor = lipgloss.Color(LightGreyHexCode)
+	WhiteColor     = lipgloss.Color(WhiteHexCode)
+
+	RedColor    = lipgloss.Color(RedHexCode)
+	OrangeColor = lipgloss.Color(OrangeHexCode)
+	YellowColor = lipgloss.Color(YellowHexCode)
+	GreenColor  = lipgloss.Color(GreenHexCode)
+	BlueColor   = lipgloss.Color(BlueHexCode)
+	PurpleColor = lipgloss.Color(PurpleHexCode)
+
+	LightGreenColor   = lipgloss.Color(LightGreenHexCode)
+	DarkGreenColor    = lipgloss.Color(DarkGreenHexCode)
+	LightestBlueColor = lipgloss.Color(LightestBlueHexCode)
+	LightBlueColor    = lipgloss.Color(LightBlueHexCode)
+	DarkBlueColor     = lipgloss.Color(DarkBlueHexCode)
+)
